39d3e7fd6a5a6263721d27b53530350e44e9efba
HoodsDream <idinkyy@gmail.com>
2015-04-08 15:03:10 -0700

    initial commit

--- SwipeAndShake/ViewController.swift
+++ SwipeAndShake/ViewController.swift
@@ -10,9 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var swipeRight = UISwipeGestureRecognizer(target: self, action: "swipe:")
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        var swipeLeft = UISwipeGestureRecognizer(target: self, action: "swipe:")
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.Left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+    }
+    
+    func swipe(gesture:UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+           
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.Right:
+                println("user swiped right")
+            case UISwipeGestureRecognizerDirection.Left:
+                println("user swiped left")
+            default:
+                break
+            }
+            
+        }
+        
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
+        
+        if event.subtype == UIEventSubtype.MotionShake {
+            println("user has shaked device")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
